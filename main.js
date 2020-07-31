8cb1d7b5d8721cf237580a3d5350dc2f59ef1ece
kimballtyler <kimballstyler@gmail.com>
2020-07-30 21:18:35 -0400

    Completed average grade

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
+var headerElement = document.querySelector("header");
+var pageHeader = new PageHeader(headerElement);
 var tableElement = document.getElementById("table");
 var gradeTable = new GradeTable(tableElement);
-var app = new App(gradeTable);
+var app = new App(gradeTable, pageHeader);
 app.start();
